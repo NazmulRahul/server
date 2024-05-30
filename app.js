f23737ff3a3bf63688fa761e2bf7cd6e11bffe39
Nazmul Hossen Rahul <69730793+nazmul-2019331047@users.noreply.github.com>
2024-05-30 20:52:03 +0600

    test

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 const express = require("express");
 const axios = require("axios");
-const dotenv=require('dotenv').config()
+const dotenv = require("dotenv").config();
 const app = express();
-const port = 3000; 
-const nazmul=require('./routes/nazmul')
-const mamun=require('./routes/mamun')
-const tanvir=require('./routes/tanvir')
-const naila=require('./routes/naila')
-const nusrat=require('./routes/nusrat')
-const samim=require('./routes/samim')
+const port = 3000;
+const nazmul = require("./routes/nazmul");
 
 app.use(express.json());
-app.use('/nazmul',nazmul)
+app.use("/nazmul", nazmul);
 
-app.listen(port,process.env.IP, () => {
+app.listen(port, process.env.IP, () => {
     console.log(`Server running on port ${port}`);
 });
