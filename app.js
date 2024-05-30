85189a1f961c5e4d007ba19b109d6279024ef160
Nazmul Hossen Rahul <69730793+nazmul-2019331047@users.noreply.github.com>
2024-05-30 20:42:58 +0600

    test

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const dotenv=require('dotenv').config()
 const app = express();
 const port = 3000; 
 const nazmul=require('./routes/nazmul')
+const mamun=require('./routes/mamun')
+const tanvir=require('./routes/tanvir')
+const naila=require('./routes/naila')
+const nusrat=require('./routes/nusrat')
+const samim=require('./routes/samim')
 
 app.use(express.json());
 app.use('/nazmul',nazmul)
